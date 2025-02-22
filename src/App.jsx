6b5c6db790489d7ae8af100a12c868e6d99154d6
mohima-mimino11 <mohimahoque22@gmail.com>
2025-02-22 17:52:06 +0600

    axios data fetch and show and react loader spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import BarChart from './components/BarChart/BarChart'
 import LineChart from './components/LineChart/LineChart'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
       <BarChart></BarChart>
+      <Phones></Phones>
       
     </>
   )
